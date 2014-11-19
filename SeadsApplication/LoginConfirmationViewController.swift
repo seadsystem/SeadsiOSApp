efde30c4d54b848b44923d8e9cc060b267dcb369
Brittany Arthur <brittany_arthur@hotmail.com>
2014-11-19 15:37:54 -0800

    data is passed between segues. multiple view controllers.

---
+++ SeadsApplication/LoginConfirmationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginConfirmationViewController.swift
+//  SeadsApplication
+//
+//  Created by Brittany Hope Arthur on 11/19/14.
+//  Copyright (c) 2014 Brittany Hope Arthur. All rights reserved.
+//
+
+import UIKit
+
+class LoginConfirmationViewController: NSObject {
+   
+}
--- SeadsApplication/ViewController.swift
+++ SeadsApplication/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var UsernameTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBAction func RegistrationDirectionsButton(sender: UIButton) {
+        return
+        
+    }
+    @IBAction func VerifyLoginCredentials(sender: AnyObject)
+    {
+        //temp for testing
+        var user = "brit"
+        var password = "seads"
+        if(UsernameTextField.text == user && PasswordTextField.text == password)
+        {
+            println("Credentials were correct");
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }else{
+            println("Credentials were not correct");
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +38,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var nextVC: LoginVC = segue.destinationViewController as LoginVC
+        
+        nextVC.recievedText = UsernameTextField.text
+    }
+    
 
 
 }
