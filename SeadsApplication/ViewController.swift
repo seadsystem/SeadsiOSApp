5637ba258701f7a18988eb1846a62e289205fa7d
Brittany Arthur <brittany_arthur@hotmail.com>
2014-11-18 19:29:28 -0800

    Initial Commit

---
+++ SeadsApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeadsApplication
+//
+//  Created by Brittany Hope Arthur on 11/18/14.
+//  Copyright (c) 2014 Brittany Hope Arthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
